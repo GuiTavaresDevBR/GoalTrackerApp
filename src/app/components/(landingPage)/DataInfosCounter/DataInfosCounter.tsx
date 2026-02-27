a2bf30555f1d711f6f7c9770751cefa7554cbf26
Guilherme Tavares <guilhermetavares@MacBook-Pro-de-Guilherme.local>
2026-02-27 19:59:30 -0300

    FEATURE: Implementar mais bastante layout da landing page

--- src/app/components/(landingPage)/DataInfosCounter/DataInfosCounter.tsx
+++ src/app/components/(landingPage)/DataInfosCounter/DataInfosCounter.tsx
@@ -1,17 +1,26 @@
+"use client";
 import style from "./DataInfosCounter.module.css";
+import CountUp from "react-countup";
+
 export default function DataInfosCounter() {
   return (
     <div className={style.container}>
       <div className={style.singleCounterContainer}>
-        <h2>+1.200 </h2>
+        <h2>
+          <CountUp end={1200} duration={1} enableScrollSpy scrollSpyOnce />
+        </h2>
         <p>Usuários ativos</p>
       </div>
       <div className={style.singleCounterContainer}>
-        <h2>+10.000</h2>
+        <h2>
+          <CountUp end={10000} duration={1} enableScrollSpy scrollSpyOnce />
+        </h2>
         <p>Hábitos criados</p>
       </div>
       <div className={style.singleCounterContainer}>
-        <h2>+200</h2>
+        <h2>
+          <CountUp end={200} duration={1} enableScrollSpy scrollSpyOnce />
+        </h2>
         <p>Feedbacks Positivos</p>
       </div>
     </div>
--- src/app/components/(landingPage)/NewsLetter/NewsLetter.tsx
+++ src/app/components/(landingPage)/NewsLetter/NewsLetter.tsx
@@ -1,8 +1,32 @@
 import style from "./NewsLetter.module.css";
+import { Mail } from "lucide-react";
+
 export default function NewsLetter() {
   return (
     <div className={style.container}>
-      <h2>NEWSLETTER. IMAGEM CHATGPT.</h2>
+      <div className={style.leftSide}>
+        <div className={style.imageContainer}>
+          <Mail size={80} color="gray" />
+        </div>
+        <div className={style.txtContainer}>
+          <h3> NEWSLETTER</h3>
+          <h2>Receba conteúdos inéditos e novidades gratuitamente</h2>
+        </div>
+      </div>
+      <div className={style.rightSide}>
+        <div className={style.upSide}>
+          <input type="text" placeholder="Digite seu e-mail" />
+          <button>Enviar</button>
+        </div>
+        <div className={style.downSide}>
+          <input type="checkbox" />
+          <small>
+            Autorizo o envio de comunicações por e-mail ou qualquer outro meio e
+            concordo com os Termos e Políticas de privacidade.
+          </small>
+        </div>
+      </div>
+      <div className={style.rightSide}></div>
     </div>
   );
 }
